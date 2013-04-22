1140c33c0bf515cac08d7f7a6a33d42333a6c268
jdadsit <jd.adsit@gmail.com>
2013-04-21 23:49:10 -0400

    added a reset button to canvas version

--- tic_tac_toe/src/CanvasGame.js
+++ tic_tac_toe/src/CanvasGame.js
@@ -6,30 +6,46 @@ define(['TicTac'], function(Logic){
         ticTac.newGame();
         var theCanvas = document.getElementById("canvasContainer");
         var context = theCanvas.getContext("2d");
-        context.fillStyle = "#ffffaa";
-        context.fillRect(0, 0, 300, 300);
         boxes = [];
         for (var i = 0; i < 3; i++) {
-            boxes.push([0, 100, i * 100, 100 + i * 100]) 
-            boxes.push([100, 200, i * 100, 100 + i * 100]) 
-            boxes.push([200, 300, i * 100, 100 + i * 100])
+            boxes.push([0, i * 100, 100, 100 + i * 100]) 
+            boxes.push([100, i * 100, 200, 100 + i * 100]) 
+            boxes.push([200, i * 100, 300, 100 + i * 100])
         };
 
-        var drawLines = function(startX, startY, endX, endY) {
+        var clearBoard = function() {
+            context.fillStyle = "#ffffaa";
+            context.fillRect(0, 0, 300, 300);
+            context.fillRect(350, 50, 115, 40);
+            resetButton = [350, 50, 465, 90];
+            context.font = "20px _sans";
+            context.textBaseline = "top";
             context.fillStyle = "#000000";
-            context.beginPath();
-            context.moveTo(startX, startY);
-            context.lineTo(endX, endY);
-            context.stroke();
+            context.fillText("Reset game", 360, 60);
+
+            var drawLines = function(startX, startY, endX, endY) {
+                context.fillStyle = "#000000";
+                context.beginPath();
+                context.moveTo(startX, startY);
+                context.lineTo(endX, endY);
+                context.stroke();
+            };
+            drawLines(100, 0, 100, 300);
+            drawLines(200, 0, 200, 300);
+            drawLines(0, 100, 300, 100);
+            drawLines(0, 200, 300, 200);
         };
-        drawLines(100, 0, 100, 300);
-        drawLines(200, 0, 200, 300);
-        drawLines(0, 100, 300, 100);
-        drawLines(0, 200, 300, 200);
+        clearBoard();
 
         var doMouseDown = function(event) { 
             x = event.pageX;
             y = event.pageY;
+
+            if((x > resetButton[0] && x < resetButton[2]) && (y > resetButton[1] && y < resetButton[3])){
+                ticTac.newGame();
+                clearBoard();
+            };
+
             while(!ticTac.playerTwo.hasWon && ticTac.available.length != 0){
                 var box = whichBox(x, y) + 1;
                 if(ticTac.available.indexOf(box) < 0) {
@@ -66,7 +82,7 @@ define(['TicTac'], function(Logic){
 
         var whichBox = function(clickedX, clickedY) {
             for(i = 0; i < 9; i++){
-                if((clickedX > boxes[i][0] && clickedX < boxes[i][1]) && (clickedY > boxes[i][2] && clickedY < boxes[i][3])){
+                if((clickedX > boxes[i][0] && clickedX < boxes[i][2]) && (clickedY > boxes[i][1] && clickedY < boxes[i][3])){
                     return i;
                 };
             };
